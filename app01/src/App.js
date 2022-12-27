c5019aec64672ebc485b11b23fcb47df1b9b0213
Nathália Zuza <nathalia555zuza@gmail.com>
2022-12-27 10:00:59 -0300

    state

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import Header from './componentes/Header'
-import Corpo from './componentes/Corpo'
+import React,{useState} from 'react';
+import Numero from './componentes/Numero'
+
+// import Header from './componentes/Header'
+// import Corpo from './componentes/Corpo'
 
 export default function App(){
 
+  const [num,setNum]=useState(10)
+
+  let n1=50
+
+
+
   return(
    <>
-      <Header/>
-      <Corpo/>
+      <p>Valor do state num em App: {num}</p>
+      <Numero num={num} setNum={setNum}/>
    </>
   )
 }

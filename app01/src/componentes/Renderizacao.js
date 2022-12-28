82f8b9a2405530662c03112554b7dba0790fa888
Nathália Zuza <nathalia555zuza@gmail.com>
2022-12-28 09:01:55 -0300

    cor

--- app01/src/componentes/Renderizacao.js
+++ app01/src/componentes/Renderizacao.js
@@ -4,6 +4,8 @@ export default function Renderizacao() {
 
     // UseState. Primeiro coloca a variável, depois onde será setada essa variável.
     const [log,SetLog]=useState(false) 
+    const [cor,setCor]=useState(1)
+
 
     // Renderização condicional de hora (retorna "bom dia", "boa tarde" ou "boa noite" dependendo do horário da máquina)
     const cumprimento=()=>{
@@ -26,15 +28,41 @@ export default function Renderizacao() {
         return 'Favor logar'
     }
 
+    // Renderização condicional de cor
+    const vermelho={color:'#f00'}
+    const verde={color:'#0f0'}
+    const azul={color:'#00f'}
+
+    const retornaCor=(c)=>{
+        if(c==1){
+            return vermelho
+        } else if  (c==2){
+            return verde
+        } else {
+            return azul
+        }
+    }
+    const mudaCor=()=>{
+        setCor(cor+1)
+        if(cor > 2){
+            setCor(1)
+        }
+    }
+
+
     // Imprime na tela
     return (
-        <>
+        <div style={{textAlign:'center', padding:'4%', fontFamily:'Segoe UI'}}>
             <h2>Cumprimento</h2>
             <p>{cumprimento()}</p>
 
             <h2>Login</h2>
             <p>{log?msgLogin():msgLogoff()}</p>
             <button onClick={()=>SetLog(!log)}>{log?'Logoff':'Login'}</button>
-        </>
+
+            <h2>Cores</h2>
+            <p style={retornaCor(cor)}>Zuza</p>
+            <button onCLick={()=>mudaCor()}>Muda cor</button>
+        </div>
     )
 }

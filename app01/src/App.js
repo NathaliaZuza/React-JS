3c44bbc4369d0c7c8a1e53294d03ae0e65d32b8b
Nathália Zuza <nathalia555zuza@gmail.com>
2022-12-29 10:06:14 -0300

    useEffect

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,21 +1,66 @@
-import React,{useState,useEffect} from 'react'
+
+import React,{useState, useEffect} from 'react'
+
 
 export default function App(){
 
-  const [contagem,setContagem]=useState(0)
+  const [count, setCount]=useState(0)
+  const [countB, setCountB]=useState(10)
+  const [user, setUser]=useState()
 
-  useEffect(
-    ()=>{
-      console.log("Página carregada")
-      document.title= 'Contagem: '+contagem
+  // Roda toda vez que a página é atualizada, pois não foi atribuido uma condição de dependência a ele
+  useEffect(() =>{
+    console.log("Roda a cada renderização")
+    console.log(user)
+  })
+
+  // Roda apenas quando o count for chamado, independente se a página atualizou ou não
+  useEffect(() =>{
+    console.log("Só roda ao implementar valor")
+  }, [count])
+
+   // Só executa quando a página  é carregada
+  useEffect(() =>{
+    console.log("Só executa uma vez")
+  }, [])
+
+  // clean up function
+  useEffect(()=>{
+    const timer = setTimeout(() => {
+      console.log(`O incrementador foi alterado ${count} vezes`)
+    }, 2000)
+    return () => {
+      clearTimeout(timer)
     }
-  )
+  }, [count])
 
-  return( 
+  // Fetch pra pegar api
+  useEffect(() =>{
+    fetch(`http://api.github.com/users/NathaliaZuza`)
+    .then((res) => res.json())
+    .then((json) => setUser(json))
+  },  [])
+
+
+
+  return(
     <>
-    <p>Contagem: {contagem}</p>
-    <button onClick={()=>setContagem(contagem+1)}>Contar</button>
+      <button onClick={() =>setCount(prevCount => prevCount + 1)}>Renderizar</button>
+      <p>{count}</p> <br/>
+
+      <button onClick={() =>setCountB(prevCount => prevCount + 1)}>Renderizar B</button>
+      <p>{countB}</p>
+
+      {user  && (
+        <div>
+          <p>Meus dados: </p>
+          <h3>{user.name}</h3>
+          <p>
+            Site:<p>{user.public_repos}</p>
+          </p>
+          <img src={user.avatar_url}></img>
+        </div>
+       )}
     </>
   )
 }
-

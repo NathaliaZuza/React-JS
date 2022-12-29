5bf3087fdc4f4e8f8e9f2825fa9c0d71b4b2e5a4
Nathália Zuza <nathalia555zuza@gmail.com>
2022-12-28 21:52:40 -0300

    map

--- app01/src/componentes/Listas.js
+++ app01/src/componentes/Listas.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 export default function Lista(){
 
-    const carros=['Golf','HRV','Focus','Cruze','Argo','Gol','Polo',]
+    // const carros=['Golf','HRV','Focus','Cruze','Argo','Gol','Polo',]
+    const carros=[
+        {categoria: "Esporte", preco:"110000.00", modelo:"Gof GTI"},
+        {categoria: "Esporte", preco:"120000.00", modelo:"Camaro"},
+        {categoria: "SUV", preco:"85000.00", modelo:"HRV"},
+        {categoria: "SUV", preco:"82000.00", modelo:"T-Cross"},
+        {categoria: "Utilitário", preco:"120000.00", modelo:"Hillux"},
+        {categoria: "Utilitário", preco:"90000.00", modelo:"Ranger"},
+    ]
     const listaCarros=carros.map( (c)=>
-    
-     <p>{c}</p>
+     <p>{c.categoria} - R$ {c.preco} - {c.modelo}</p>
     )
     return(
 
